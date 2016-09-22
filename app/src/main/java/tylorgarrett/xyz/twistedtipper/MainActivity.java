e294326948873f6482e08b0c2f2ee5aae63b2e95
Tylor Garrett <tylorgarrett@gmail.com>
2016-09-21 22:20:47 -0400

    added changes

--- app/src/main/java/tylorgarrett/xyz/twistedtipper/MainActivity.java
+++ app/src/main/java/tylorgarrett/xyz/twistedtipper/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.github.lzyzsd.circleprogress.DonutProgress;
@@ -20,24 +19,50 @@ public class MainActivity extends AppCompatActivity implements CritiqueAdapter.O
 
     List<Critique> critiqueList;
 
+    int totalPercentage;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         critiqueList = new LinkedList<>();
         mockCritiqueData();
-
+        totalPercentage = 50;
+        donutProgress.setProgress((int) totalPercentage);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new CritiqueAdapter(this, critiqueList, this));
     }
 
     private void mockCritiqueData(){
         critiqueList.add(new Critique("Server got my order wrong!", Critique.Value.RED));
-        critiqueList.add(new Critique("Waiting too long for refill", Critique.Value.YELLOW));
+        critiqueList.add(new Critique("Waiting too long!", Critique.Value.YELLOW));
         critiqueList.add(new Critique("Server was prompt and on time!", Critique.Value.GREEN));
     }
 
     @Override public void onCritiqueClicked(Critique critique) {
-        Toast.makeText(MainActivity.this, critique.getName(), Toast.LENGTH_SHORT).show();
+        Critique.Value value = critique.getValue();
+        int change = 0;
+        switch (value){
+            case GREEN:
+                change = 15;
+                break;
+            case YELLOW:
+                change = -5;
+                break;
+            case RED:
+                change = -15;
+                break;
+        }
+        totalPercentage = totalPercentage + change;
+        setDonutProgress(totalPercentage);
+    }
+
+    public void setDonutProgress(int val){
+        if ( val < 1 ){
+            totalPercentage = 0;
+        } else if ( val > 99 ) {
+            totalPercentage = 100;
+        }
+        donutProgress.setProgress(totalPercentage);
     }
 }
